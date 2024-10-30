8fcef36baac4bda636c389f2eeaf867523b59a36
siddhesh-100 <sssiddheshhh@gmail.com>
2024-10-31 00:34:37 +0530

    Added CP Template

---
+++ A.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <time.h>
+#include <iomanip>
+#include <string>
+#include <limits>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <sstream>
+#include <cctype>
+#include <bitset>
+using namespace std;
+using ll = long long;
+void solve(){
+    
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t; cin >> t; while(t--)
+    solve();
+    return 0;
+}
